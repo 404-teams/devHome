e469ec9e27235235dc10c937d35e84ca30936fa7
YazanAlaiwah <eng.yazanalaiwah@gmail.com>
2020-04-18 17:41:28 +0300

    tesst

--- server/authorization.js
+++ server/authorization.js
@@ -1,24 +1,21 @@
 const jwt = require('jsonwebtoken');
-const Auth = {};
-Auth.makeToken = function (user) {
-  return jwt.sign({ user ,expiresIn:'30s' }, 'devhome'); 
+
+module.exports = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(' ')[1];
+    const decodedToken = jwt.verify(token, 'devhome');
+    res.send(decodedToken)
+  //   const userId = decodedToken.userId;
+  //   if (req.body.userId && req.body.userId !== userId) {
+  //     throw 'Invalid user ID';
+  //   } else {
+  //     next();
+  //   }
+  } catch {
+    res.status(401).json({
+      error: new Error('Invalid request!')
+    });
+  }
 };
 
-// module.exports = (req, res, next) => {
-//   try {
-//     const token = req.headers.authorization.split(' ')[1];
-//     const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
-//     const userId = decodedToken.userId;
-//     if (req.body.userId && req.body.userId !== userId) {
-//       throw 'Invalid user ID';
-//     } else {
-//       next();
-//     }
-//   } catch {
-//     res.status(401).json({
-//       error: new Error('Invalid request!')
-//     });
-//   }
-// };
-
-module.exports = Auth;
+// module.exports = Auth;
--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ app.set('view engine', 'ejs');
 
 
 app.get('/cdns',API.cdnFunction);
-app.get('/cdns/search' , API.searchOfCdn);
+app.get('/cdns/search' ,Auth, API.searchOfCdn);
 app.post('/cdns/save' , API.saveDataForCdnToDataBase);
 app.get('/cdns/user/:user_id' , API.getEachUserCdnData);
 app.post('/signup',API.signup)
