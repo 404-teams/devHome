26ab538a086e704fe6d1e1cfcdf3a46465723211
Balqeesqasem <balqeesmohammadq.95@gmail.com>
2020-04-18 13:35:42 +0300

    make a get request in the server with rout

--- server/apis.js
+++ server/apis.js
@@ -1,5 +1,23 @@
 const DB = require('./database')
 const Obj = require('./constructor')
 const Auth = require('./authorization')
+const superagent = require('superagent')
 const API = {};
 module.exports = API;
+
+API.cdnFunction = function(req,res) {
+getCdn()
+.then( results => {
+    res.send({cdns:results});
+    // res.render('views/cdns', {cdns:results});
+})
+}
+function getCdn(){
+    let url = `https://api.cdnjs.com/libraries?fields=version,description,author,filename,keywords`;
+    return superagent.get(url)
+.then(cdnData => {
+    return cdnData.body.results.map( val =>{
+    return new Obj.Cdn(val) ;
+    })
+})}
+
--- server/constructor.js
+++ server/constructor.js
@@ -1,3 +1,14 @@
 const Object = {};
 
+
+Object.Cdn = function (val){
+    this.name= val.name;
+    this.latest = val.latest;
+    this.version=val.version;
+    this.description=val.description;
+    this.author=val.author;
+    this.filename=val.filename;
+    this.keywords=val.keywords;
+}
+
 module.exports = Object;
