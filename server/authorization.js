e2dc9bcb87dbbde23762427f53ed8e613d986336
Abdallah <eng.abdallahobaid@gmail.com>
2020-04-22 12:56:44 +0300

    22/04/2020 12:55pm css work Q/A pages

--- server/authorization.js
+++ server/authorization.js
@@ -6,35 +6,22 @@ module.exports = (req, res, next) => {
     // take the baerer auth from the header
     const bearerHeader = req.headers.authorization;
     // check if there is a value
-
-    if (bearerHeader !== 'bearer undefined') {
+    if (bearerHeader !== undefined) {
       /// take the token from the header
       let decodedToken = bearerHeader.split(' ');
       let token = decodedToken[1];
       ///// verify the token that its not expired and take the id user from it
-
-      const userData = jwt.verify(token, 'devhome');
-      //// check if the user is the same for the request
-      if (userData.id === Number(req.params.id)) {
-        //// allow the process
-        next();
-      } else {
-        res.send('not the same user');
-      }
-    } else {
-      res.send('please login or singup');
-
-      //       jwt.verify(token, 'devhome', (error,data) =>{
-      //         if(error){
-      //           res.status(404).send('token is end');
-      //         }
-      //         req.body.id = data.id;
-      //         return next();
-      //       });
+      jwt.verify(token, 'devhome', (error,data) =>{
+        if(error){
+          res.status(404).send('token is end');
+        }
+        req.body.id = data.id;
+        return next();
+      });
     }
   } catch {
     res.status(401).json({
-      error: new Error('Invalid request!'),
+      error: new Error('Invalid request!')
     });
   }
 };
